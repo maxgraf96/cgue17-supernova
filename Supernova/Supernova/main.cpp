4816535db190b05531a2c11ef0dba994b09a8f00
Fabian Fröschl <e1552749@student.tuwien.ac.at>
2017-04-03 16:10:06 +0200

    basic game loop

--- Supernova/Supernova/main.cpp
+++ Supernova/Supernova/main.cpp
@@ -1,5 +1,107 @@
-#include<iostream>
+#include<iostream>	
+#include<glew.h>
+#include<glfw3.h>
+
+void init(GLFWwindow* window);
+void update(float time_delta);
+void draw();
+void cleanup();
+
+const int width = 1280;
+const int height = 720;
 
 void main() {
-	std::cout << "Hola, beetches!" << std::endl;
+
+	if (!glfwInit()) {
+		std::cerr << "ERROR: Could not init glfw" << std::endl;
+		system("PAUSE");
+		exit(EXIT_FAILURE);
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	auto window = glfwCreateWindow(width, height, "Test Window", nullptr, nullptr); //TODO: chagnge title
+
+	if (!window) {
+		std::cerr << "ERROR: Could not open window" << std::endl;
+		glfwTerminate;
+		system("PAUSE");
+		exit(EXIT_FAILURE);
+	}
+
+	glfwMakeContextCurrent(window);
+
+	glewExperimental = GL_TRUE;
+
+	if (glewInit() != GLEW_OK) {
+		std::cerr << "ERROR: Could not initialize glew." << std::endl;
+		glfwTerminate;
+		system("PAUSE");
+		exit(EXIT_FAILURE);
+	}
+
+	init(window);
+
+	bool running = true;
+	auto time = glfwGetTime();
+
+	while (running && !glfwWindowShouldClose(window)) {
+
+		//cleare frame and depth buffer
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		//compute frame time delta
+		auto time_new = glfwGetTime();
+		auto time_delta = (float)(time_new - time);
+		time = time_new;
+
+		std::cout << "frametime:" << time_delta * 1000 << "ms =~" << 1.0 / time_delta << "fps" << std::endl;
+
+		//react to user input
+		glfwPollEvents();
+
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE)) {
+			running = false;
+			glfwSetWindowShouldClose(window, true);
+		}
+
+		//update game components
+		update(time_delta);
+
+		//draw game components
+		draw();
+		glfwSwapBuffers(window);
+
+		//catch errors
+		if (glGetError() != GL_NO_ERROR)
+		{
+			std::cerr << "GL ERROR DETECTED!" << std::endl;
+		}
+	}
+
+	//cleanup!
+	cleanup();
+	glfwTerminate();
+
+}
+
+void init(GLFWwindow* window) {
+	glfwSetWindowTitle(window, "Supernova");
+
+	glClearColor(0.35f, 0.36f, 0.43f, 0.3f);
+	glViewport(0, 0, width, height);
+}
+
+void update(float time_delta) {
+
+}
+
+void draw() {
+
+}
+
+void cleanup() {
+
 }
